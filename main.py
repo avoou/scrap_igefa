10ad863ba8f093689e243266fee0f70113e19c7c
avoou <xxxenodochialritchie@gmail.com>
2024-10-28 19:02:10 +0200

    refactor code

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import aiofiles
 import aiohttp
 import asyncio
 import json
+import os
 
 
 #TODO:
@@ -14,15 +15,18 @@ import json
 # get all items from all pages for each category 
 # update df
 
+#https://api.igefa.de/shop/v1/products/by-variant/XVznJhi5M3mqCyBt2XKdr3
+#https://api.igefa.de/shop/v1/products?filter%5Btaxonomy%5D=UZ58DPNjGf6axF3MRtAw6Q
+#https://api.igefa.de/shop/v1/products?limit=20&page=1&filter%5Btaxonomy%5D=axwgQ9VwrwBYWYLCkp2oKj&requiresAggregations=0&track=1
 
 TARGET_URL = 'https://store.igefa.de'
-CATEGORIES_API_SLUG = 'f4SXre6ovVohkGNrAvh3zR'
+CATEGORIES_API_ID = 'f4SXre6ovVohkGNrAvh3zR'
 API_URL = 'https://api.igefa.de/shop/v1/taxonomies'
 
 
-async def get_categories(api_url: str, id: str, gathered_json: dict, session: aiohttp.ClientSession, count: list) -> None:
+async def get_category(api_url: str, category_id: str, gathered_json: dict, session: aiohttp.ClientSession, count: list) -> None:
     
-    link = f"{api_url}/{id}"
+    link = f"{api_url}/{category_id}"
     
     async with session.get(link) as response:
         count[0] += 1
@@ -42,29 +46,58 @@ async def get_categories(api_url: str, id: str, gathered_json: dict, session: ai
         
         if categories_json.get("children"):
             tasks = [
-                get_categories(api_url, child["id"], gathered_json, session, count)
+                get_category(api_url, child["id"], gathered_json, session, count)
                 for child in categories_json["children"]
             ]
             await asyncio.gather(*tasks)
 
 
-async def main():
+async def gather_all_categories(api_url: str, category_id: str) -> dict:
     count=[0]
     gathered_json = {}
 
     async with aiohttp.ClientSession() as session:
-        await get_categories(
-            api_url=API_URL,
-            id=CATEGORIES_API_SLUG,
+        await get_category(
+            api_url=api_url,
+            category_id=category_id,
             gathered_json=gathered_json,
             session=session,
             count=count
         )
+    
+    print(f'There has been gathered {count} categories') #483
 
-    async with aiofiles.open('gathered_links.json', 'w') as f:
-        await f.write(json.dumps(gathered_json, ensure_ascii=False))
+    return gathered_json
 
-    print(count) #483
+
+def make_links(gathered_json: dict):
+    
+    for category, info in gathered_json.items():
+        category_id = info["id"]
+        items_by_category_url = f'https://api.igefa.de/shop/v1/products?filter%5Btaxonomy%5D={category_id}'
+
+
+async def main():
+    print("Step: 1 \nGathering all categories of items...")
+
+    output_filename = 'gathered_links.json'
+
+    if not os.path.exists(output_filename):
+        gathered_json = await gather_all_categories(
+            api_url=API_URL,
+            category_id=CATEGORIES_API_ID
+        )
+        async with aiofiles.open(output_filename, 'w') as f:
+            await f.write(json.dumps(gathered_json, ensure_ascii=False))
+    print(f'Step: 2 \nSave list of gategories to a {output_filename} file')
+    
+    print("Step: 3 \nGetting links of items by category")
+    
+    with open(output_filename, 'r') as f:
+        gathered_json = json.load(f)
+
+
+    
 
 if __name__ == '__main__':
     asyncio.run(main())
